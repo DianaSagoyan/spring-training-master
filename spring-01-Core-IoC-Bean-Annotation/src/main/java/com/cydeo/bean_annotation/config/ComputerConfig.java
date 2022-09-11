d945d77644d22e5d976cb90c6c81f839d7ea90a9
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-11 15:24:33 -0500

    Added a new method with @Bean annotation

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -1,7 +1,14 @@
 package com.cydeo.bean_annotation.config;
 
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ComputerConfig {
+    @Bean
+    public Monitor monitorSony(){
+        return new SonyMonitor("25 inch", "Acer", 25);
+    }
 }
