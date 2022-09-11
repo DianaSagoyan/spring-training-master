b83c7c4e4efaac8d6fb1a8a4083f008f5b101d2f
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-11 15:30:42 -0500

    Created new methods for case and motherboard

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/config/ComputerConfig.java
@@ -1,7 +1,11 @@
 package com.cydeo.bean_annotation.config;
 
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.casefactory.DellCase;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +13,15 @@ import org.springframework.context.annotation.Configuration;
 public class ComputerConfig {
     @Bean
     public Monitor monitorSony(){
-        return new SonyMonitor("25 inch", "Acer", 25);
+        return new SonyMonitor("25 inch", "Sony", 25);
     }
+    @Bean
+    public Case caseDell(){
+        return new DellCase("22OB", "Dell", "240");
+    }
+    @Bean
+    public Motherboard motherboardAsus(){
+        return new AsusMotherboard("BJ-200", "Asus", 4, 6, "v2.44");
+    }
+
 }
