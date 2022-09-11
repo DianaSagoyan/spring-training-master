1bbd8571a46c076009d9c4ea23ecb00b68a9485f
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-11 15:52:24 -0500

    Added configs to the container

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,10 +1,23 @@
 package com.cydeo.bean_annotation;
 
+import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.config.ComputerConfig;
+import com.cydeo.bean_annotation.config.StringIntConfig;
+import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
     public static void main(String[] args) {
-        ApplicationContext container = new AnnotationConfigApplicationContext(); //Creating container
+        ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, StringIntConfig.class); //Creating container
+
+        Monitor theMonitor = container.getBean(Monitor.class);
+        Case theCase = container.getBean(Case.class);
+        Motherboard theMotherboard = container.getBean(Motherboard.class);
+
+
+        PC myPc = new PC(theCase, theMonitor, theMotherboard);
+        myPc.powerUp();
     }
 }
