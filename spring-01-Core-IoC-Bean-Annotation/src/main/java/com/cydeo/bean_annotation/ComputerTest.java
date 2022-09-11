e243cc5947d4d943cd30ca027d26af5d1517a4e7
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-11 14:58:04 -0500

    Created new ComputerTest class and created a container

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -0,0 +1,10 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComputerTest {
+    public static void main(String[] args) {
+        ApplicationContext container = new AnnotationConfigApplicationContext(); //Creating container
+    }
+}
