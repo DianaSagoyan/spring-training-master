cc7e58a488f5636a394f413a74d590432879a232
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-11 21:00:59 -0500

    Added configs to the container

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -4,6 +4,7 @@ import com.cydeo.bean_annotation.casefactory.Case;
 import com.cydeo.bean_annotation.config.ComputerConfig;
 import com.cydeo.bean_annotation.config.StringIntConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
+import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
