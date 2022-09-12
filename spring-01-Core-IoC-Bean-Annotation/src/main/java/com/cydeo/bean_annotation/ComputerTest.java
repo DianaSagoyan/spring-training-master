c846417d655372c6d8bf9b79947c4c188a721d1f
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-12 11:50:13 -0500

    Review

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -1,17 +1,30 @@
 package com.cydeo.bean_annotation;
 
 import com.cydeo.bean_annotation.casefactory.Case;
+import com.cydeo.bean_annotation.casefactory.DellCase;
 import com.cydeo.bean_annotation.config.ComputerConfig;
 import com.cydeo.bean_annotation.config.StringIntConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
+import com.cydeo.bean_annotation.motherboardfactory.AsusMotherboard;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
     public static void main(String[] args) {
         ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, StringIntConfig.class); //Creating container
+
+        BeanFactory context = new AnnotationConfigApplicationContext();
+
+        System.out.println("Retrieving the beans");
+
+        SonyMonitor sony = container.getBean(SonyMonitor.class);
+        DellCase dell = container.getBean(DellCase.class);
+        AsusMotherboard asus = container.getBean(AsusMotherboard.class);
+        
+        PC myPc2 =new PC(dell, sony, asus);
 
         Monitor theMonitor = container.getBean(Monitor.class);
         Case theCase = container.getBean(Case.class);
