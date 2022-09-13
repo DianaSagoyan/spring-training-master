406f831ebe062a65d88626bf326b1f966472a348
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-13 15:14:49 -0500

    Practicing DI

--- Spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
+++ Spring-05-Core-Abstraction/src/main/java/com/cydeo/CommentApp.java
@@ -1,7 +1,21 @@
 package com.cydeo;
 
+import com.cydeo.config.CommentConfig;
+import com.cydeo.model.Comment;
+import com.cydeo.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class CommentApp {
     public static void main(String[] args) {
-        
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(CommentConfig.class);
+
+        CommentService commentService = context.getBean(CommentService.class);
+        commentService.publishComment(comment);
     }
 }
--- Spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
+++ Spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -5,10 +5,17 @@ import com.cydeo.proxy.CommentNotificationProxy;
 import com.cydeo.proxy.EmailCommentNotificationProxy;
 import com.cydeo.repository.CommentRepository;
 import com.cydeo.repository.DBCommentRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CommentService {
-    private CommentRepository commentRepository;
-    private CommentNotificationProxy emailCommentNotificationProxy;
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy emailCommentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, CommentNotificationProxy emailCommentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.emailCommentNotificationProxy = emailCommentNotificationProxy;
+    }
 
     public void publishComment(Comment comment){
         //save in DB
