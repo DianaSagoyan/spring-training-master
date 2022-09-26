e0e7558f82df9a9f0c68118107ff988e1bc425d7
DianaSagoyan <dianasagoyan@yahoo.com>
2022-09-26 16:36:44 -0500

    Practicing Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -5,6 +5,8 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -21,4 +23,12 @@ public class MentorController {
         model.addAttribute("mentor", new Mentor());
         return "mentor/mentor-register";
     }
+
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor, Model model){
+
+        return "redirect:/mentor/register";
+//        return "mentor/mentor-confirmation";
+    }
+
 }
