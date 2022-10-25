e82fed5c87900faa9a961f3834084f0e2d266821
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-25 18:01:03 -0500

    Completed changes

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
@@ -2,6 +2,8 @@ package com.cydeo;
 
 //import org.flywaydb.core.Flyway;
 //import org.flywaydb.core.api.output.MigrateResult;
+import org.flywaydb.core.Flyway;
+import org.flywaydb.core.api.output.MigrateResult;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,9 +18,9 @@ public class Spring13DataCinemaLabApplication {
         SpringApplication.run(Spring13DataCinemaLabApplication.class, args);
     }
 
-//    @Bean
-//    public MigrateResult migrateResult(DataSource dataSource){
-//        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
-//    }
+    @Bean
+    public MigrateResult migrateResult(DataSource dataSource){
+        return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+    }
 
 }
