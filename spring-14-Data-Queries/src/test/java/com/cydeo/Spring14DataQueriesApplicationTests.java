4f7d04dbfe1b26e87f5d9db347b4e821a127111a
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-27 11:21:29 -0500

    Starting new module Data Queries

--- spring-14-Data-Queries/src/test/java/com/cydeo/Spring14DataQueriesApplicationTests.java
+++ spring-14-Data-Queries/src/test/java/com/cydeo/Spring14DataQueriesApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Spring14DataQueriesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
